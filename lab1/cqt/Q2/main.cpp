b9ff148b1960f201c1ddaa5d3c0b0e47cbbdca36
ApocalypseFallSpring <162588886+ApocalypseFallSpring@users.noreply.github.com>
2025-09-28 13:07:32 +0800

    Create main.cpp

---
+++ lab1/cqt/Q2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a, b, i, t, n;
+    a = 0;
+    b = 1;
+    i = 1;
+    cin >> n;
+    cout << a << endl;
+    cout << b << endl;
+    while (i < n)
+    {
+        t = b;
+        b = add(a, b);
+        if (b % 2 == 0)
+            cout << b << endl;
+        a = t;
+        i = i + 1;
+    }
+}
